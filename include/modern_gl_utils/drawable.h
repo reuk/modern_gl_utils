e391c46529db461e010526bcea99bd213f3ad696
reuk <reuben.thomas@me.com>
2016-06-08 17:10:48 +0100

    updated base classes with defaults

--- include/modern_gl_utils/drawable.h
+++ include/modern_gl_utils/drawable.h
@@ -2,6 +2,12 @@
 
 class Drawable {
 public:
+    Drawable() = default;
+    Drawable(const Drawable&) = default;
+    Drawable& operator=(const Drawable&) = default;
+    Drawable(Drawable&&) noexcept = default;
+    Drawable& operator=(Drawable&&) noexcept = default;
     virtual ~Drawable() noexcept = default;
+
     virtual void draw() const = 0;
 };
--- include/modern_gl_utils/updatable.h
+++ include/modern_gl_utils/updatable.h
@@ -2,6 +2,12 @@
 
 class Updatable {
 public:
+    Updatable() = default;
+    Updatable(const Updatable&) = default;
+    Updatable& operator=(const Updatable&) = default;
+    Updatable(Updatable&&) noexcept = default;
+    Updatable& operator=(Updatable&&) noexcept = default;
     virtual ~Updatable() noexcept = default;
+
     virtual void update(float dt) = 0;
 };
